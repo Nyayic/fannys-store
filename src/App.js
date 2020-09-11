2a7a9f304284961ed2602277c7adcc633f653b6a
Nyayic <chrosbiefannie@gmail.com>
2020-09-11 14:59:41 +0300

    modify app.js file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h4>Welcome to my shop</h4>
+     <h4>Welcome to my shop my beautiful customers</h4>
     </div>
   );
 }
 
 export default App;
+
+
